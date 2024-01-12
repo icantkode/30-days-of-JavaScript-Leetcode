5a0dd76eeeff3a06f7aa832584180c6c8f885d2a
icantkode <ashersiddique127@gmail.com>
2024-01-12 17:45:32 +0500

    2665

---
+++ 2665. Counter II.js
@@ -0,0 +1,26 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function (init) {
+  let currVal = init
+  return {
+    increment: () => {
+      return ++currVal
+    },
+    decrement: () => {
+      return --currVal
+    },
+    reset: () => {
+      currVal = init
+      return currVal
+    }
+  }
+}
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
